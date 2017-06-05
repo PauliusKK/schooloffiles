67fadaba972a8608337d40968ef52ebd4b7653f7
Paulius Kašiuba <p.kasiuba@gmail.com>
2017-06-05 11:37:17 +0300

    added main.js

--- js/main.js
+++ js/main.js
@@ -7,16 +7,121 @@ $( document ).ready(function() {
         });
     }
 
-    // function changeFooterPosition() {
-    //     $screenHeight = $(window).height();
-    //     $bodyHeight = $('body').height();
-    //     $headerHeight = $('header.header-started').height();
+    function fixContent() {
+        $('.surgery_signup').css({ maxWidth: '980px' });
 
-    //     if( $screenHeight > ($bodyHeight + $headerHeight) ) {
-    //         $('footer.footer-started').css({ position: 'absolute', bottom: 0, width: '100%' });
-    //         $('body').css({ backgroundColor: '#f3f3f3' });
-    //     }
-    // }
+        $('.question-but, #value_bottom-signup_but').each(function() {
+            $button = $(this);
+            $text = $button.text();
+            $button.empty();
+
+            if( $button.attr('id') == "value_bottom-signup_but" ) {
+                // $button.append('<a>' + $text + '</a>').removeAttr('class').addClass('question-but black').css({
+                //     boxShadow: '0 2px 4px 0 rgba(0, 0, 0, 0.41)'
+                //     backgroundColor: '#000',
+                //     border: 'none',
+                //     borderRadius: '11px',
+                //     display: 'block',
+                //     margin: '0 auto',
+                //     padding: '10px 0'
+                // });
+
+                // $button.addClass('main-button');
+
+                // $button.find('a').css({
+                //     boxShadow: '0px 10px 0px 0px #b21515',
+                //     fontSize: '25px',
+                //     lineHeight: '2.08',
+                //     borderRadius: '11px',
+                //     color: 'white',
+                //     padding: '10px 10px',
+                //     fontWeight: 800,
+                //     letterSpacing: '0.7px',
+                //     display: 'block',
+                //     width: '100%',
+                //     textTransform: 'uppercase',
+                //     textDecoration: 'none',
+                //     whiteSpace: 'nowrap'
+                // });
+
+            } else {
+                $button.append('<a>' + $text + '</a>').removeAttr('class').addClass('question-but').css({
+                    boxShadow: '0 2px 4px 0 rgba(0, 0, 0, 0.41)'
+                    backgroundColor: '#cd3135',
+                    border: 'none',
+                    borderRadius: '11px',
+                    display: 'block',
+                    margin: '0 auto',
+                    padding: '10px 0'
+                });
+
+                $button.addClass('main-button');
+
+                $button.find('a').css({
+                    boxShadow: '0px 10px 0px 0px #B4181C', // #b21515
+                    fontSize: '25px',
+                    lineHeight: '2.08',
+                    borderRadius: '11px',
+                    color: 'white',
+                    padding: '10px 10px',
+                    fontWeight: 800,
+                    letterSpacing: '0.7px',
+                    display: 'block',
+                    width: '100%',
+                    textTransform: 'uppercase',
+                    textDecoration: 'none',
+                    whiteSpace: 'nowrap'
+                });
+            }
+
+            // $button.append('<a>' + $text + '</a>').removeAttr('class').addClass('question-but').css({
+            //     boxShadow: '0 2px 4px 0 rgba(0, 0, 0, 0.41)',
+            //     backgroundColor: '#cd3135',
+            //     border: 'none',
+            //     borderRadius: '11px',
+            //     display: 'block',
+            //     margin: '0 auto',
+            //     padding: '10px 0'
+            // });
+
+            // $button.addClass('main-button');
+
+            // $button.find('a').css({
+            //     boxShadow: '0px 10px 0px 0px #b21515',
+            //     fontSize: '25px',
+            //     lineHeight: '2.08',
+            //     borderRadius: '11px',
+            //     color: 'white',
+            //     padding: '10px 120px',
+            //     fontWeight: 800,
+            //     letterSpacing: '0.7px',
+            //     display: 'block',
+            //     width: '100%',
+            //     textTransform: 'uppercase',
+            //     textDecoration: 'none',
+            //     whiteSpace: 'nowrap'
+            // });
+
+        });
+
+        $('.content, .surgery-content').each(function() {
+            $content = $(this);
+            $parent = $content.parent();
+            $marginNext = $parent.next().css('margin-top');
+            $marginPrev = $parent.prev().css('margin-top');
+
+            $content.css({ width: '100%', maxWidth: '980px', boxShadow: '0 2px 4px 0 rgba(226, 226, 226, 0.5)', border: 'solid 1px #f7f7f7' });
+            $parent.css({ margin: 0, padding: '9em 0', background: '#f3f3f3' });
+            $parent.parent().css({ marginTop: 0 });
+
+            if( $marginNext = 252 ) $parent.next().hide();
+            else if( $marginPrev = 252 ) $parent.prev().hide();
+
+            $('footer.footer-started').show();
+        });
+
+        $('#about_body-content').css({ display: 'inherit' });
+    }
 
     // Changing sections height property in coming-soon page
     function changeSectionHeight() {
@@ -63,7 +168,7 @@ $( document ).ready(function() {
 
     moveArrow();
     changeSectionHeight();
-    // changeFooterPosition();
+    fixContent();
 
     $('header.bigger button.home').click(function() {
         $('nav.menu-appear').toggleClass('active');
